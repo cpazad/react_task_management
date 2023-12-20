fb050b80f8921ada8299519a4892e942e7b08ec0
cpazad <cpazad@gmail.com>
2023-12-21 03:58:02 +0600

    Task Managaement Platform app initiated, footer set

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
